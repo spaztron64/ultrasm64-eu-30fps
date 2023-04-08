fa393182a61ae923715e61c44a68531cb64ff783
Fazana <52551480+FazanaJ@users.noreply.github.com>
2023-04-08 20:14:10 +0100

    fix tree shadows

--- levels/bob/areas/1/3/model.inc.c
+++ levels/bob/areas/1/3/model.inc.c
@@ -127,6 +127,7 @@ const Gfx bob_seg7_dl_0700A470[] = {
     gsDPPipeSync(),
     gsDPSetCombineMode(G_CC_MODULATEIA, G_CC_PASS2),
     gsSPClearGeometryMode(G_LIGHTING),
+    gsDPSetEnvColor(255, 255, 255, 255),
     gsDPSetTile(G_IM_FMT_IA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD),
     gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
     gsDPTileSync(),
